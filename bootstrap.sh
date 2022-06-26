7a7672cae7e87cdad79a94f49b3a85b08b8015ed
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-26 22:33:22 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo pip install opencv-python
+sudo pip install boto3
+sudo pip install pillow
+sudo python3 -m pip install opencv-python
+sudo python3 -m pip install boto3
+sudo python3 -m pip install pillow
